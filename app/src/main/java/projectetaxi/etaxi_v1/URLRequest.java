472ed6c97697d3caa040980fdc2541127a3976a7
Jharana Gurung <jharna.915@gmail.com>
2017-10-15 10:26:52 +0545

    7th commit.it is about maps maintenance.

--- app/src/main/java/projectetaxi/etaxi_v1/URLRequest.java
+++ app/src/main/java/projectetaxi/etaxi_v1/URLRequest.java
@@ -6,9 +6,9 @@ package projectetaxi.etaxi_v1;
 
 public class URLRequest {
 
-    public static String driverLoginRequest = "http://192.168.100.11:8000/api/v1/driver/login";
-    public static String driverRegisterRequest = "http://192.168.100.11:8000/api/v1/drivers";
-    public static String passengerLoginRequest = "http://192.168.100.11:8000/api/v1/passenger/login";
-    public static String passengerRegisterRequest = "http://192.168.100.11:8000/api/v1/passengers";
+    public static String driverLoginRequest = "http://192.168.100.5:8000/api/v1/driver/login";
+    public static String driverRegisterRequest = "http://192.168.100.5:8000/api/v1/drivers";
+    public static String passengerLoginRequest = "http://192.168.100.5:8000/api/v1/passenger/login";
+    public static String passengerRegisterRequest = "http://192.168.100.5:8000/api/v1/passengers";
 
 }
