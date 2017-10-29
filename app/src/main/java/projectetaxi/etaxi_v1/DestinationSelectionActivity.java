d352e3185dd4ae2ce6b1caf45efac71a142afe7c
Jharana Gurung <jharna.915@gmail.com>
2017-10-29 10:14:30 +0545

    source and destination snapping

--- app/src/main/java/projectetaxi/etaxi_v1/DestinationSelectionActivity.java
+++ app/src/main/java/projectetaxi/etaxi_v1/DestinationSelectionActivity.java
@@ -4,17 +4,20 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -33,8 +36,18 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -110,10 +123,15 @@ public class DestinationSelectionActivity extends FragmentActivity implements
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     GoogleApiClient mGoogleApiClient;
+    ArrayList<LatLng> MarkerPoints;
+
     Location mLastLocation;
     Marker mCurrLocationMarker, marker;
     LocationRequest mLocationRequest;
     private GoogleMap mMap;
+    TextView tvDistanceDuration;
+
+    static LatLng currentlatLng;
 
     final String TAG = this.getClass().getName();
 
@@ -127,6 +145,11 @@ public class DestinationSelectionActivity extends FragmentActivity implements
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
         }
+        tvDistanceDuration = (TextView) findViewById(R.id.tv_distance_time);
+        // Initializing
+        MarkerPoints = new ArrayList<LatLng>();
+
+
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager()
                         .findFragmentById(R.id.map);
@@ -300,6 +323,8 @@ public class DestinationSelectionActivity extends FragmentActivity implements
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,
                     this);
         }
+        // Adding new item to the ArrayList
+        MarkerPoints.add(latLng);
     }
 
 
@@ -395,10 +420,10 @@ public class DestinationSelectionActivity extends FragmentActivity implements
 
 
     @Override
-    public void onMapLongClick(final LatLng latLng) {
+    public void onMapLongClick(final LatLng destlatLng) {
 
-        final String lat = String.valueOf(latLng.latitude);
-        final String lng = String.valueOf(latLng.longitude);
+        final String lat = String.valueOf(destlatLng.latitude);
+        final String lng = String.valueOf(destlatLng.longitude);
 
         destinationLat = lat;
         destinationLng = lng;
@@ -406,20 +431,22 @@ public class DestinationSelectionActivity extends FragmentActivity implements
         Log.d(TAG, "--------Dest Latitude: " + destinationLat);
         Log.d(TAG, "--------Dest Longitude: " + destinationLng);
 
-        Log.d(TAG, "--------Long Clicked lat before if statement: " + latLng.latitude);
-        Log.d(TAG, "--------Long Clicked lng before if statement: " + latLng.longitude);
+        Log.d(TAG, "--------Long Clicked lat before if statement: " + destlatLng.latitude);
+        Log.d(TAG, "--------Long Clicked lng before if statement: " + destlatLng.longitude);
 
-        doubleDestinationLat = latLng.latitude;
-        doubleDestinationLng = latLng.longitude;
+        doubleDestinationLat = destlatLng.latitude;
+        doubleDestinationLng = destlatLng.longitude;
 
-        Log.d(TAG, "Long Clicked latlng: " + latLng);
+        Log.d(TAG, "Long Clicked latlng: " + destlatLng);
 
         if(marker!=null){
             marker.remove();
 
-            Log.d(TAG, "--------After if statement, Long Clicked lat: " + latLng.latitude);
-            Log.d(TAG, "--------After if statement, Long Clicked latlng: " + latLng.longitude);
-            Log.d(TAG, "After if stmt, Long Clicked latlng: " + latLng);
+            Log.d(TAG, "--------After if statement, Long Clicked lat: " + destlatLng.latitude);
+            Log.d(TAG, "--------After if statement, Long Clicked latlng: " + destlatLng.longitude);
+            Log.d(TAG, "After if stmt, Long Clicked latlng: " + destlatLng);
+
+            mMap.clear();
         }
 
         final Button btSelectDestination = (Button) findViewById(R.id.btSelectDest);
@@ -445,9 +472,40 @@ public class DestinationSelectionActivity extends FragmentActivity implements
 
 
 
-        marker= mMap.addMarker(new MarkerOptions().position(latLng).title(latLng.toString()));
+        marker= mMap.addMarker(new MarkerOptions().position(destlatLng).title(destlatLng.toString()));
+
+        // Getting URL to the Google Directions API
+        String url = getUrl(currentlatLng, destlatLng);
+        Log.d("onMapClick", url.toString());
+        FetchUrl FetchUrl = new FetchUrl();
+
+        // Start downloading json data from Google Directions API
+        FetchUrl.execute(url);
     }
 
+    private String getUrl(LatLng currentlatLng, LatLng destlatLng) {
+        // Origin of route
+        String str_origin = "origin=" + currentlatLng.latitude + "," + currentlatLng.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + destlatLng.latitude + "," + destlatLng.longitude;
+
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+
+        return url;
+    }
 
 
     @Override
@@ -459,4 +517,172 @@ public class DestinationSelectionActivity extends FragmentActivity implements
         Log.d(TAG, "/////--Dest Lat: " + destinationLat);
         Log.d(TAG, "/////--Dest Lng: " + destinationLng);
     }
+    /**
+     * A method to download json data from url
+     */
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+            Log.d("downloadUrl", data.toString());
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    // Fetches data from url passed
+    private class FetchUrl extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try {
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+                Log.d("Background Task data", data.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+
+        }
+    }
+
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                Log.d("ParserTask",jsonData[0].toString());
+                DataParser parser = new DataParser();
+                Log.d("ParserTask", parser.toString());
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+                Log.d("ParserTask","Executing routes");
+                Log.d("ParserTask",routes.toString());
+
+            } catch (Exception e) {
+                Log.d("ParserTask",e.toString());
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points;
+            PolylineOptions lineOptions = null;
+            String distance = "";
+            String duration = "";
+
+
+
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    if(j==0){    // Get distance from the list
+                        distance = (String)point.get("distance");
+                        continue;
+                    }else if(j==1){ // Get duration from the list
+                        duration = (String)point.get("duration");
+                        continue;
+                    }
+
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(10);
+                lineOptions.color(Color.BLUE);
+
+                Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+            }
+
+            tvDistanceDuration.setText("Distance:"+distance + ", Duration:"+duration);
+
+
+            // Drawing polyline in the Google Map for the i-th route
+            if(lineOptions != null) {
+                mMap.addPolyline(lineOptions);
+            }
+            else {
+                Log.d("onPostExecute","without Polylines drawn");
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 }
