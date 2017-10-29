e4d329a767d618adda4c89bd8c455280c57419b9
Aashim Bajracharya <ashimbazracharya@gmail.com>
2017-10-29 14:20:26 +0545

    Changes in URL...

--- app/src/main/java/projectetaxi/etaxi_v1/URLRequest.java
+++ app/src/main/java/projectetaxi/etaxi_v1/URLRequest.java
@@ -6,8 +6,10 @@ package projectetaxi.etaxi_v1;
 
 public class URLRequest {
 
-    private static int num1 = 100;
-    private static int num2 = 4;
+    private static int num1 = 192;
+    private static int num2 = 168;
+    private static int num3 = 100;
+    private static int num4 = 26;
 
     private static String lat;
     private static String lng;
@@ -21,12 +23,18 @@ public class URLRequest {
         URLRequest.lng = lng;
     }
 
-    public static String driverLoginRequest = "http://192.168."+num1+"."+num2+":8000/api/v1/driver/login";
-    public static String driverRequest = "http://192.168."+num1+"."+num2+":8000/api/v1/drivers";
-    public static String passengerLoginRequest = "http://192.168."+num1+"."+num2+":8000/api/v1/passenger/login";
-    public static String passengerRequest = "http://192.168."+num1+"."+num2+":8000/api/v1/passengers";
-    public static String bookingHistoryRequest = "http://192.168."+num1+"."+num2+":8000/api/v1/taxi_booking/history";
-    public static String bookingRequest = "http://192.168."+num1+"."+num2+":8000/api/v1/taxi_bookings";
+    public static String driverLoginRequest = "http://192.168."+num1+"."+num2+"."+num3+"."+num4
+            +":8000/api/v1/driver/login";
+    public static String driverRequest = "http://192.168."+num1+"."+num2+"."+num3+"."+num4
+            +":8000/api/v1/drivers";
+    public static String passengerLoginRequest = "http://192.168."+num1+"."+num2+"."+num3+"."
+            +num4+":8000/api/v1/passenger/login";
+    public static String passengerRequest = "http://192.168."+num1+"."+num2+"."+num3+"."
+            +num4+":8000/api/v1/passengers";
+    public static String bookingHistoryRequest = "http://192.168."+num1+"."+num2+"."+num3+"."+num4
+            +":8000/api/v1/taxi_booking/history";
+    public static String bookingRequest = "http://192.168."+num1+"."+num2+"."+num3+"."+num4
+            +":8000/api/v1/taxi_bookings";
 
     public static String getAddressFromLatlongUrl() {
 
